4b984c408d9270a5ef7d1c5b6044f6b335470fa7
jvanderbijl2004 <38768256+jvanderbijl2004@users.noreply.github.com>
2025-03-19 01:06:36 +0100

    created group activity with notes, flashcards and sets fragments, implemented tab view although it doesnt fully work yet: has 6 tabs instead of 3 which breaks the view

---
+++ app/src/main/java/nl/tue/appdev/studie/GroupActivity.java
@@ -0,0 +1,46 @@
+package nl.tue.appdev.studie;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import nl.tue.appdev.studie.ui.main.SectionsPagerAdapter;
+import nl.tue.appdev.studie.databinding.ActivityGroupBinding;
+
+public class GroupActivity extends AppCompatActivity {
+
+    private ActivityGroupBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityGroupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fab;
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null)
+                        .setAnchorView(R.id.fab).show();
+            }
+        });
+    }
+}
