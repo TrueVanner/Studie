04ca8d67198fa2c27f5d00dc62970d6ea346b623
Merlin Belmon <m.k.belmon@student.tue.nl>
2025-03-29 19:06:07 +0100

    Updated "secure" pw requirements and added custom message for insecure passwords. Commented out code for trying to get uniqueness on usernames is also left in.

--- app/src/main/java/nl/tue/appdev/studie/SignupActivity.java
+++ app/src/main/java/nl/tue/appdev/studie/SignupActivity.java
@@ -10,17 +10,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -29,6 +36,9 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
     private EditText nameInput;
     private EditText emailInput;
     private EditText passwordInput;
+    private boolean usernameAvailable = true;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +63,7 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         login.setOnClickListener(this);
     }
 
-    enum UpdateType { BAD_NAME, BAD_PASSWORD, BAD_EMAIL, BAD_AUTH }
+    enum UpdateType { BAD_NAME, BAD_PASSWORD, BAD_EMAIL, BAD_AUTH, INSECURE_PW, USERNAME_TAKEN }
 
     private void updateUI(UpdateType updateType) {
         switch(updateType) {
@@ -70,6 +80,11 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
                 Toast.makeText(SignupActivity.this, "Authentication failed.",
                         Toast.LENGTH_SHORT).show();
                 break;
+            case USERNAME_TAKEN:
+                nameInput.setError("This username is already used by another user.");
+                break;
+            case INSECURE_PW:
+                passwordInput.setError("Password is insecure.");
         }
     }
 
@@ -78,28 +93,48 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         int id = v.getId();
         Intent toHome = new Intent(SignupActivity.this, HomeActivity.class);
         Intent tologin = new Intent(SignupActivity.this, LoginActivity.class);
-        Log.d(TAG, "test1");
         if (id == R.id.button_sign_up) {
             String name = nameInput.getText().toString();
             String email = emailInput.getText().toString();
             String password = passwordInput.getText().toString();
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
 
             // Check if fields are correct
             if (name.isBlank()) {
                 updateUI(UpdateType.BAD_NAME);
                 return;
             }
+            if (password.isBlank()) {
+                updateUI((UpdateType.BAD_PASSWORD));
+                return;
+            }
             if(!email.toLowerCase().matches("[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,6}")) {
                 updateUI(UpdateType.BAD_EMAIL);
                 return;
             }
-            if (password.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$")) {
-                updateUI(UpdateType.BAD_PASSWORD);
+            if (!password.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{10,}$")) {
+                updateUI(UpdateType.INSECURE_PW);
                 return;
             }
 
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
-            Log.d(TAG, "test2");
+            // Check if email and username are already used
+//            db.collection("users").whereEqualTo("name", name).get()
+//                            .addOnCompleteListener(this,task -> {
+//                                if (task.isSuccessful()) {
+//                                    for (QueryDocumentSnapshot document : task.getResult()) {
+//                                        Log.d(TAG, document.getId() + " => " + document.getData());
+//                                        usernameAvailable = false;
+//                                    }
+//                                } else {
+//                                    Log.d(TAG, "Query failed");
+//                                }
+//                            });
+//
+//            if (!usernameAvailable) {
+//                updateUI(UpdateType.USERNAME_TAKEN);
+//                return;
+//            }
+
             mAuth.createUserWithEmailAndPassword(email.trim(), password.trim())
                     .addOnCompleteListener(this, task -> {
                         if (task.isSuccessful()) {
