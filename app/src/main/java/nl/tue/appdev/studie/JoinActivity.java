edf5570fda9114a9f3f39881a7b2f06a837ac508
Merlin Belmon <m.k.belmon@student.tue.nl>
2025-03-30 15:08:50 +0200

    Fixed bug where joinActivity displays all groups instead of only unjoined grous. Also refactored code.

--- app/src/main/java/nl/tue/appdev/studie/JoinActivity.java
+++ app/src/main/java/nl/tue/appdev/studie/JoinActivity.java
@@ -5,10 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +14,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -24,29 +21,23 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.google.zxing.qrcode.encoder.QRCode;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Vector;
-//import java.util.logging.Logger;
 
 public class JoinActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "JoinActivity";
 
-    private FirebaseAuth mAuth;
 
-    private HashMap<String, String> groups = new HashMap<String, String>(); // ID, name
+
+    private HashMap<String, String> groups = new HashMap<>(); // ID, name
     private HashMap<String, String> currGroups = new HashMap<>(); // ID, name
-    private EditText searchbar;
     private String query;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // TODO refactor code to make less complex
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_join);
@@ -57,7 +48,6 @@ public class JoinActivity extends AppCompatActivity implements View.OnClickListe
         });
 
         // Create a group view fragment
-        FragmentManager fragmentManager = getSupportFragmentManager();
         JoinGroupviewFragment groupview = new JoinGroupviewFragment();
 
         // Add back button
@@ -67,6 +57,7 @@ public class JoinActivity extends AppCompatActivity implements View.OnClickListe
         ImageButton qr = findViewById(R.id.qr_icon_button);
         qr.setOnClickListener(this);
 
+        EditText searchbar;
         // Add searchbar
         searchbar = findViewById(R.id.searchbar_join);
         searchbar.setOnKeyListener((v, keyCode, event) -> {
@@ -82,7 +73,7 @@ public class JoinActivity extends AppCompatActivity implements View.OnClickListe
             return false;
         });
 
-        mAuth = FirebaseAuth.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         FirebaseUser user = mAuth.getCurrentUser();
@@ -99,6 +90,7 @@ public class JoinActivity extends AppCompatActivity implements View.OnClickListe
                     assert userDocument != null;
                     currGroups = (HashMap<String, String>) userDocument.get("groups");
                     Log.d(TAG, String.valueOf(currGroups));
+                    createFragment(groupview);
                 } else {
                     Log.d(TAG, "No such document");
                 }
@@ -107,6 +99,25 @@ public class JoinActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.join_back_button) {
+            Intent toHome = new Intent(JoinActivity.this, HomeActivity.class);
+            startActivity(toHome);
+        } else if (id == R.id.qr_icon_button) {
+            Intent toQR = new Intent(JoinActivity.this, ScannerActivity.class);
+            startActivity(toQR);
+        }
+    }
+
+    private void createFragment(JoinGroupviewFragment groupview) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         CollectionReference collectionRef = db.collection("groups");
         collectionRef.get().addOnCompleteListener(this, task -> {
             if (task.isSuccessful()) {
@@ -139,23 +150,5 @@ public class JoinActivity extends AppCompatActivity implements View.OnClickListe
                 Log.d(TAG, "get failed with ", task.getException());
             }
         });
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        if (id == R.id.join_back_button) {
-            Intent toHome = new Intent(JoinActivity.this, HomeActivity.class);
-            startActivity(toHome);
-        } else if (id == R.id.qr_icon_button) {
-            Intent toQR = new Intent(JoinActivity.this, ScannerActivity.class);
-            startActivity(toQR);
-        }
     }
 }
