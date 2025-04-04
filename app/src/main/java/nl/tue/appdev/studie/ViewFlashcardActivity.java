f7e5f2ab0b535f527d708d1cc448f2219d1f4ab2
Deniz Seker <sekerdeniz@gmail.com>
2025-04-04 23:28:53 +0200

    More comments added to 4 classes regarding flashcard view, flashcard set view/creation/viewport
    Took 17 seconds

--- app/src/main/java/nl/tue/appdev/studie/ViewFlashcardActivity.java
+++ app/src/main/java/nl/tue/appdev/studie/ViewFlashcardActivity.java
@@ -27,7 +27,6 @@ public class ViewFlashcardActivity extends AppCompatActivity implements View.OnC
     private static final String TAG = "ViewFlashcardActivity";
 
     private String groupId;
-
     private String flashcardId;
     private String questionText;
     private String answerText;
@@ -42,32 +41,33 @@ public class ViewFlashcardActivity extends AppCompatActivity implements View.OnC
         setContentView(R.layout.activity_flashcard_view);
 
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        if (mAuth.getCurrentUser() == null) { // redirect to login screen if user is not logged in
+        // redirect to login screen if user is not logged in
+        if (mAuth.getCurrentUser() == null) {
             Intent loginIntent = new Intent(this, LoginActivity.class);
             startActivity(loginIntent);
+            // disposal of the flashcard view
             finish();
         }
-
+        // assignation of the screen elements in flashcard view
         flashcardStateTextView = findViewById(R.id.question_text);
         flipButton = findViewById(R.id.fc_view_flip_button);
         backButton = findViewById(R.id.fc_view_back_button);
-
+        // buttons onClick handlers
         flipButton.setOnClickListener(this);
         backButton.setOnClickListener(this);
 
         Intent intent = getIntent();
-
+        // retrieve the id of the group that the flashcard is in
         groupId = intent.getStringExtra("group_id");
+        // retrieve the id of the flashcard to be viewed
         flashcardId = intent.getStringExtra("flashcard_id");
 
+        // prevention of null fetches from the database
         if (flashcardId != null) {
             loadFlashcardData(flashcardId);
         } else {
             Log.e(TAG, "No flashcard ID");
         }
-
-//        createTestFlashcard();
-
     }
 
     /**
@@ -76,12 +76,16 @@ public class ViewFlashcardActivity extends AppCompatActivity implements View.OnC
      * @param flashcardId The ID of the flashcard to be loaded.
      */
     private void loadFlashcardData(String flashcardId) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();  // firestore instance
+        // get firestore instance for fetching data
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // get the flashcard data from the database
         db.collection("flashcards")
                 .document(flashcardId).get().addOnSuccessListener(documentSnapshot -> {
                     if (documentSnapshot.exists()) {
+                        // store the question and answer fields of the flashcard
                         questionText = documentSnapshot.getString("question");
                         answerText = documentSnapshot.getString("answer");
+                        // set the new fragment view
                         FlashcardFragment flashcardFragment = FlashcardFragment.newInstance(questionText, answerText);
                         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_flashcard, flashcardFragment).commit();
                     } else {
@@ -91,28 +95,18 @@ public class ViewFlashcardActivity extends AppCompatActivity implements View.OnC
                 .addOnFailureListener(e -> Log.e(TAG, "Firestore error: " + e.getMessage()));
     }
 
-
-    /**
-     * TESTING: A test method to create a local flashcard to display in the activity fragment.
-     */
-    private void createTestFlashcard() {
-        String testFlashcardId = "test_fc_1";
-        String testQuestionText = "Test Question?";
-        String testAnswerText = "Test Answer.";
-
-        FlashcardFragment flashcardFragment = FlashcardFragment.newInstance(testQuestionText, testAnswerText);
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_flashcard, flashcardFragment).commit();
-    }
-
     /**
      * Flips the flashcard between the question and answer viewports.
      */
     private void flipFlashcard() {
+        // retrieve flashcard viewport fragment
         FlashcardFragment flashcardFragment = (FlashcardFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_flashcard);
+        // update boolean variables
         isShowingQuestion = !isShowingQuestion;
         if (flashcardFragment != null) {
+            // display the question or the answer
             flashcardFragment.updateFlashcardContent(isShowingQuestion);
+            // update the flashcard state text at the top of the view
             if (isShowingQuestion) {
                 flashcardStateTextView.setText("Question");
             } else {
@@ -129,11 +123,14 @@ public class ViewFlashcardActivity extends AppCompatActivity implements View.OnC
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.fc_view_flip_button) {
+            // 'flips' the currently viewed flashcard, swapping between 'question' and 'answer' sides
             flipFlashcard();
         } else if (v.getId() == R.id.fc_view_back_button) {
+            // returns back to the group screen of the group the set is in by passing the intent with the id of the group to the GroupActivity
             Intent intent = new Intent(ViewFlashcardActivity.this, GroupActivity.class);
             intent.putExtra("id", groupId);
             startActivity(intent);
+            // disposal of the flashcard view
             finish();
         }
     }
